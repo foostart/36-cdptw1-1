e5bc2e5012d4502168a3866478d0b869d63e461b
buixuantu <buixuantu@users.noreply.github.com>
2019-09-26 11:45:30 +0700

    1378

---
+++ 1378/js/1378.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+$(function () {  
+$("#datepicker").datepicker({         
+autoclose: true,         
+todayHighlight: true 
+}).datepicker('update', new Date());
+});
+</script>
