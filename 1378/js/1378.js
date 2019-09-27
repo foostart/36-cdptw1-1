9afaf90e28acbc32f3fb4f705d047c74ffa858d5
buixuantu <buixuantu@users.noreply.github.com>
2019-09-27 21:07:21 +0700

    Update 1378.js

--- 1378/js/1378.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-$(function () {  
-$("#datepicker").datepicker({         
-autoclose: true,         
-todayHighlight: true 
-}).datepicker('update', new Date());
-});
-</script>
