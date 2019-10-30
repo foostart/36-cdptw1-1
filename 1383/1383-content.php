6113b54a92b3133d372fee5f16da54154642e4b5
buixuantu <buixuantu@users.noreply.github.com>
2019-10-30 10:35:25 +0700

    upadate-1383

--- 1383/1383-content.php
+++ 1383/1383-content.php
@@ -1,14 +1,13 @@
-  <?php
-  $url_host = 'http://' . $_SERVER['HTTP_HOST'];
-  $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
-  $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+   <?php
+   $url_host = 'http://' . $_SERVER['HTTP_HOST'];
+   $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+   $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
 
-  preg_match_all($pattern_uri, __DIR__, $matches);
-  $url_path = $url_host . $matches[1][0];
-  $url_path = str_replace('\\', '/', $url_path);
-  ?>
-
-  <div class="type-1383">
+   preg_match_all($pattern_uri, __DIR__, $matches);
+   $url_path = $url_host . $matches[1][0];
+   $url_path = str_replace('\\', '/', $url_path);
+   ?>
+   <div class="type-1383">
     <div class="container">
       <h2 class="h2">Looking For Answers?</h2>
       <div class="row">
@@ -17,12 +16,13 @@
             <div class="panel panel-default">
               <div class="panel-heading" role="tab" id="headingOne">
                 <h4 class="panel-title">
-                  <a class="active" data-toggle="collapse" data-parent="#accordion_1" href="#accordion_1_collapseOne" aria-expanded="true" aria-controls="accordion_1_collapseOne">
+                  <a class="click" data-toggle="collapse" data-parent="#accordion_1" href="#accordion_1_collapseOne" aria-expanded="true" aria-controls="accordion_1_collapseOne">+</a>
+                  <a class="active" href="#accordion_1_collapseOne" >
                     Food Delivery. Order online food delivery at RoomService
                   </a>
                 </h4>
               </div>
-              <div  id="accordion_1_collapseOne" class="collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+              <div  id="accordion_1_collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne">
                 <div class="panel-body">
                   <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
                     inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
@@ -35,7 +35,8 @@
             <div class="panel panel-default">
               <div class="panel-heading" role="tab" id="headingTwo">
                 <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion_1" href="#accordion_1_collapseTwo" aria-expanded="false" aria-controls="accordion_1_collapseTwo">
+                  <a class="click" data-toggle="collapse" data-parent="#accordion_1" href="#accordion_1_collapseTwo" aria-expanded="false" aria-controls="accordion_1_collapseTwo">+</a>
+                  <a class="active" href="#accordion_1_collapseTwo">
                     Non equidem invideo, miror magis posuere velit aliquet.
                   </a>
                 </h4>
@@ -53,7 +54,8 @@
             <div class="panel panel-default">
               <div class="panel-heading" role="tab" id="headingThree">
                 <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion_1" href="#accordion_1_collapseThree" aria-expanded="false" aria-controls="accordion_1_collapseThree">
+                  <a class="click" data-toggle="collapse" data-parent="#accordion_1" href="#accordion_1_collapseThree" aria-expanded="false" aria-controls="accordion_1_collapseThree">+</a>
+                  <a class="active" href="#accordion_1_collapseThree">
                     Subscribe to our monthly newsletter to get the latest scoop
                   </a>
                 </h4>
@@ -71,7 +73,8 @@
             <div class="panel panel-default">
               <div class="panel-heading" role="tab" id="headingFour">
                 <h4 class="panel-title">
-                  <a  data-toggle="collapse" data-parent="#accordion_1" href="#accordion_1_collapseFour" aria-expanded="false" aria-controls="accordion_1_collapseFour">
+                  <a class="click" data-toggle="collapse" data-parent="#accordion_1" href="#accordion_1_collapseFour" aria-expanded="false" aria-controls="accordion_1_collapseFour">+</a>
+                  <a class="active" href="#accordion_1_collapseFour">
                     CSS Powered Buttons with Hover Tooltips
                   </a>
                 </h4>
@@ -89,7 +92,8 @@
             <div class="panel panel-default">
               <div class="panel-heading" role="tab" id="headingFive">
                 <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion_1" href="#accordion_1_collapseFive" aria-expanded="false" aria-controls="accordion_1_collapseFive">
+                  <a class="click" data-toggle="collapse" data-parent="#accordion_1" href="#accordion_1_collapseFive" aria-expanded="false" aria-controls="accordion_1_collapseFive">+</a>
+                  <a class="active" href="#accordion_1_collapseFive">
                     Smashicons: 100 Free Icons
                   </a>
                 </h4>
--- 1383/1383.php
+++ 1383/1383.php
@@ -24,9 +24,9 @@ $less->compileFile('less/1383.less', 'css/1383.css');
     <title>module 1383</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>              
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./js/jquery-2.1.4.min.js"></script>              
+    <script src="./js/bootstrap.min.js"></script>
     <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet"  />
     <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet"/>
     <link href="<?php echo $url_path ?>/css/1383.css" rel="stylesheet" type="text/css" />
